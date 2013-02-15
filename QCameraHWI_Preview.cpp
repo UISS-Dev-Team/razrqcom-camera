RonGokhale/android_hardware_qcom_camera

Camera: Fix CTS testPreviewPictureSizesCombination.
CRs-Fixed: 346704

(cherry picked from commit 921dcbf654e2a1ac3ed585b15c917576814d87c0)

Change-Id: If3be171dbf06cc8fc0f3c038b1f61bac6ab45e32

QCameraHWI_Preview.cpp
@@ -88,6 +88,23 @@ status_t QCameraStream_preview::setPreviewWindow(preview_stream_ops_t* window)
     return retVal;
 }
 
+status_t QCameraStream_preview::freeBuffersBeforeStartPreview()
+{
+        status_t retVal = NO_ERROR;
+        if (mbPausedBySnapshot) {
+           mbPausedBySnapshot = FALSE;
+
+           LOGV("%s : Preview window was same but the preview dimension changed",__func__);
+           /*free camera_memory handles and return buffer back to surface*/
+           putBufferToSurface();
+           if (mDisplayBuf.preview.buf.mp != NULL) {
+               delete[] mDisplayBuf.preview.buf.mp;
+               mDisplayBuf.preview.buf.mp = NULL;
+           }
+       }
+       return retVal;
+}
+
 status_t QCameraStream_preview::getBufferFromSurface() {
     int err = 0;
     int numMinUndequeuedBufs = 0;

UISS-Dev-Team/razrqcom-camera

CRs-fixed: 346704
(cherry picked from commit 677870b791069b3b8e9aa347d6666e6f4c9581f5)
Change-Id: I6ec20790a8812dc2cae9933b29989a2f63450b16

QCameraHWI_Preview.cpp
@@ -71,6 +71,23 @@ status_t QCameraStream_preview::setPreviewWindow(preview_stream_ops_t* window)
     return retVal;
 }
 
+status_t QCameraStream_preview::freeBuffersBeforeStartPreview()
+{
+        status_t retVal = NO_ERROR;
+        if (mbPausedBySnapshot) {
+           mbPausedBySnapshot = FALSE;
+
+           ALOGV("%s : Preview window was same but the preview dimension changed",__func__);
+           /*free camera_memory handles and return buffer back to surface*/
+           putBufferToSurface();
+           if (mDisplayBuf.preview.buf.mp != NULL) {
+               delete[] mDisplayBuf.preview.buf.mp;
+               mDisplayBuf.preview.buf.mp = NULL;
+           }
+       }
+       return retVal;
+}
+
 status_t QCameraStream_preview::getBufferFromSurface() {
     int err = 0;
     int numMinUndequeuedBufs = 0;