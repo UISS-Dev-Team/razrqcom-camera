sonyxperiadev/camera

msm: camera: invalidate cached buffers before sending to hardware
Since the buffers are now allocated in cached mode they
need to be cache-invalidated before hardware writes new
data into them. This prevents the CPU from reading stale data
from the cache. Also, if hardware reads a CPU modified buffer, the
data needs to be cache-cleaned or written back before hardware
reads from it. An example here is gemini reading a post-processed
buffer for JPEG encoding.

Change-Id: I974032f09ba7321cf0687f6e5a29c6ffa23f0ea6

QCameraHWI_Still.cpp
@@ -1646,6 +1646,8 @@ encodeDisplayAndSave(mm_camera_ch_data_buf_t* recvd_frame,
 {
     status_t ret = NO_ERROR;
     struct msm_frame *postview_frame;
+    struct ion_flush_data cache_inv_data;
+    int ion_fd;
     int buf_index = 0;
     ssize_t offset_addr = 0;
     common_crop_t dummy_crop;
@@ -1661,7 +1663,36 @@ encodeDisplayAndSave(mm_camera_ch_data_buf_t* recvd_frame,
       LOGE("%s: set JPEG rotation in ZSL mode", __func__);
       mHalCamCtrl->setJpegRotation(isZSLMode());
     }
-
+#ifdef USE_ION
+    /*Clean out(Write-back) cache before sending for JPEG*/
+    memset(&cache_inv_data, 0, sizeof(struct ion_flush_data));
+    ion_fd = open("/dev/ion", O_RDONLY);
+    if(ion_fd < 0) {
+        LOGE("%s: Ion device open failed\n", __func__);
+    }
+    cache_inv_data.vaddr = (void*)recvd_frame->snapshot.main.frame->buffer;
+    cache_inv_data.fd = recvd_frame->snapshot.main.frame->fd;
+    cache_inv_data.handle = recvd_frame->snapshot.main.frame->fd_data.handle;
+    cache_inv_data.length = recvd_frame->snapshot.main.frame->ion_alloc.len;
+    if(ion_fd > 0) {
+      if(ioctl(ion_fd, ION_IOC_CLEAN_INV_CACHES, &cache_inv_data) < 0)
+          LOGE("%s: Cache Invalidate failed\n", __func__);
+      else {
+          LOGD("%s: Successful cache invalidate\n", __func__);
+	  if(!isFullSizeLiveshot()) {
+            cache_inv_data.vaddr = (void*)recvd_frame->snapshot.thumbnail.frame->buffer;
+            cache_inv_data.fd = recvd_frame->snapshot.thumbnail.frame->fd;
+            cache_inv_data.handle = recvd_frame->snapshot.thumbnail.frame->fd_data.handle;
+            cache_inv_data.length = recvd_frame->snapshot.thumbnail.frame->ion_alloc.len;
+            if(ioctl(ion_fd, ION_IOC_CLEAN_INV_CACHES, &cache_inv_data) < 0)
+              LOGE("%s: Cache Invalidate failed\n", __func__);
+            else
+              LOGD("%s: Successful cache invalidate\n", __func__);
+          }
+      }
+      close(ion_fd);
+    }
+#endif
     memset(&dummy_crop,0,sizeof(common_crop_t));
     ret = encodeData(recvd_frame, &dummy_crop, mSnapshotStreamBuf.frame_len,
                      enqueued);

UISS-Dev-Team/razrqcom-camera

Change-Id: I8b4598f7984b8292bcc7ed1cc8d7ac03dd7365a8

QCameraHWI_Still.cpp
@@ -1556,6 +1556,8 @@ encodeDisplayAndSave(mm_camera_ch_data_buf_t* recvd_frame,
 {
     status_t ret = NO_ERROR;
     struct msm_frame *postview_frame;
+    struct ion_flush_data cache_inv_data;
+    int ion_fd;
     int buf_index = 0;
     ssize_t offset_addr = 0;
     common_crop_t dummy_crop;
@@ -1571,7 +1573,36 @@ encodeDisplayAndSave(mm_camera_ch_data_buf_t* recvd_frame,
       ALOGE("%s: set JPEG rotation in ZSL mode", __func__);
       mHalCamCtrl->setJpegRotation(isZSLMode());
     }
-
+#ifdef USE_ION
+    /*Clean out(Write-back) cache before sending for JPEG*/
+    memset(&cache_inv_data, 0, sizeof(struct ion_flush_data));
+    ion_fd = open("/dev/ion", O_RDONLY);
+    if(ion_fd < 0) {
+        ALOGE("%s: Ion device open failed\n", __func__);
+    }
+    cache_inv_data.vaddr = (void*)recvd_frame->snapshot.main.frame->buffer;
+    cache_inv_data.fd = recvd_frame->snapshot.main.frame->fd;
+    cache_inv_data.handle = recvd_frame->snapshot.main.frame->fd_data.handle;
+    cache_inv_data.length = recvd_frame->snapshot.main.frame->ion_alloc.len;
+    if(ion_fd > 0) {
+      if(ioctl(ion_fd, ION_IOC_CLEAN_INV_CACHES, &cache_inv_data) < 0)
+          ALOGE("%s: Cache Invalidate failed\n", __func__);
+      else {
+          ALOGD("%s: Successful cache invalidate\n", __func__);
+	  if(!isFullSizeLiveshot()) {
+            cache_inv_data.vaddr = (void*)recvd_frame->snapshot.thumbnail.frame->buffer;
+            cache_inv_data.fd = recvd_frame->snapshot.thumbnail.frame->fd;
+            cache_inv_data.handle = recvd_frame->snapshot.thumbnail.frame->fd_data.handle;
+            cache_inv_data.length = recvd_frame->snapshot.thumbnail.frame->ion_alloc.len;
+            if(ioctl(ion_fd, ION_IOC_CLEAN_INV_CACHES, &cache_inv_data) < 0)
+              ALOGE("%s: Cache Invalidate failed\n", __func__);
+            else
+              ALOGD("%s: Successful cache invalidate\n", __func__);
+          }
+      }
+      close(ion_fd);
+    }
+#endif
     memset(&dummy_crop,0,sizeof(common_crop_t));
     mPictureFormat=CAMERA_YUV_420_NV12;
     mThumbnailFormat=CAMERA_YUV_420_NV12;